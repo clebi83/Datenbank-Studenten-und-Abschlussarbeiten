42eac6b98d6a420184d639e5d5b03dc06071ca9e
clebi83 <clemens@sanwald-online.de>
2017-12-28 16:28:53 +0100

    db verbindung geändert
    db verbindung geändert

--- dbConnect.php
+++ dbConnect.php
@@ -8,8 +8,8 @@
 //Datenbankverbindung aufbauen
 
 
-$mysqli = new mysqli('localhost', 'd028dc1b','8uw3MfYbxtsGdrxz','d028dc1b');
-//$mysqli = new mysqli('localhost', 'root','your_password','8uw3MfYbxtsGdrxz');
+//$mysqli = new mysqli('localhost', 'root','8uw3MfYbxtsGdrxz','d028dc1b');
+$mysqli = new mysqli('localhost', 'root','your_password','abschlussarbeiten');
 if($mysqli->connect_error){
     echo 'Fehler bei der Verbindung: ' . mysqli_connect_error();
     exit();
