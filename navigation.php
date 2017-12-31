034f2912126b995a15d529e1cc101bc9119a6983
clebi83 <clemens@sanwald-online.de>
2017-12-31 15:10:20 +0100

    Navigation
    Navigation bearbeitet

--- navigation.php
+++ navigation.php
@@ -5,7 +5,7 @@
  * Date: 26.11.2017
  * Time: 11:33
  */
-$navigation = ["index.php" => "Startseite", "student.php" => "Studenten", "abschlussarbeit.php" => "Abschlussarbeiten", "betreuer.php" => "Betreuer", "StudierendeAbschlussarbeit.php" => "Arbeiten vor Abgabe", "bewerteteArbeiten.php" => "Bewertete Arbeiten"];
+$navigation = ["index.php" => "Startseite", "student.php" => "Anlegen: Studenten", "abschlussarbeit.php" => "Anlegen: Abschlussarbeit", "betreuer.php" => "Anlegen: Betreuer", "StudierendeAbschlussarbeit.php" => "Ansicht: Arbeiten vor Abgabe", "bewerteteArbeiten.php" => "Ansicht: Bewertete Arbeiten"];
 
 foreach ($navigation as $nav => $titel){
     echo "<a href=$nav>$titel</a><br>";
